029baf0956bfab5f4f40d6d41ce8f7351351d5be
Michael Nnaji <mnnaji3@uwo.ca>
2020-10-11 14:51:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,7 +45,7 @@ def save
 end
 
 def self.create(attributes)
-    new_dog = Dog.new(nil,nil,nil)
+    new_dog = Dog.new(nil,"store","store")
      attributes.each {|key, value| new_dog.send(("#{key}="), value)}
     new_dog
 end
